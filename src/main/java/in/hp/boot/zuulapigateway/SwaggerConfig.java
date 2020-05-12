6ad6cad12e5d774052e80214f56e9a6002ceac8c
Hariprasath <balaprasad24@gmail.com>
2020-05-13 00:08:11 +0530

    Aggregated Swagger for downstream services

---
+++ src/main/java/in/hp/boot/zuulapigateway/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package in.hp.boot.zuulapigateway;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.zuul.filters.RouteLocator;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Added @Primary to override default SwaggerResourceProvider
+ */
+@Configuration
+@Primary
+public class SwaggerConfig implements SwaggerResourcesProvider {
+
+    @Autowired
+    private RouteLocator routeLocator;
+
+    @Override
+    public List<SwaggerResource> get() {
+        // Fetching all routes from RouteLocator and adding the Swagger url
+        return routeLocator.getRoutes().stream().map(route -> {
+            SwaggerResource resource = new SwaggerResource();
+            resource.setName(route.getId());
+            resource.setLocation(route.getFullPath().replace("**", "v2/api-docs"));
+            resource.setSwaggerVersion("2.0");
+            return resource;
+        }).collect(Collectors.toList());
+    }
+}
